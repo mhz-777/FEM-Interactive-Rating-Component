7ca14dcc471ed01cf7f385dc50546d79a9b1dfa3
Mustafa <mhassanzadeh777@gmail.com>
2022-09-28 22:56:30 -0600

    Added container for text section

--- src/components/rating.js
+++ src/components/rating.js
@@ -11,19 +11,18 @@ const Rating = () => {
     function handleSubmit () {
         if(value != null) {
             setConfirm(true);
-            console.log('yes!');
-        }else{
-            console.log('no');
         }
     }
 
     return (
         <div className="content-container">
             <img src={starIcon} alt="star icon" className='star-sprite' />
-            <h1 className="main-heading">How did we do?</h1>
-            <p className="calltoaction">
-                Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
-            </p>
+            <div className="text-container">
+                <h1 className="main-heading">How did we do?</h1>
+                <p className="description">
+                    Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+                </p>
+            </div>
             <div className="buttons-container">
                 <button className="value-btn" onClick={() => setValue(1)}>1</button>
                 <button className="value-btn" onClick={() => setValue(2)}>2</button>
@@ -31,7 +30,7 @@ const Rating = () => {
                 <button className="value-btn" onClick={() => setValue(4)}>4</button>
                 <button className="value-btn" onClick={() => setValue(5)}>5</button>
             </div>
-            <button className="submit" onClick={handleSubmit}>Submit</button>
+            <button className="submit" onClick={handleSubmit}>SUBMIT</button>
             {confirmed === true && <Confirmed selection={value}/>}
         </div>
     );
