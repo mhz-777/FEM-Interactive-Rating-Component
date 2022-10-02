08e45b996d575418172bf1c8f8a800d0a4829b07
Mustafa <mhassanzadeh777@gmail.com>
2022-10-02 13:16:14 -0600

    Completed markup for confirmed state

--- src/components/confirmed.js
+++ src/components/confirmed.js
@@ -4,7 +4,7 @@ import confirmedImage from '../assets/images/illustration-thank-you.svg'
 const Confirmed = (props) => {
     return (
       <div className="confirmed-container">
-            <img src={confirmedImage} alt="confirmed thank you" />
+            <img src={confirmedImage} alt="confirmed thank you" className='confirmed-image'/>
             <p className="dynamic-results">You selected {props.selection} out of 5</p>
             <h1 className="thanks-heading">Thank you!</h1>
             <p className="thanks-message">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
